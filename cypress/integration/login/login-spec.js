2c92dc43b8e6a139cf6b590ff44790d23f8d96e1
Ernesto <66950087+ErnstHB@users.noreply.github.com>
2021-01-15 20:25:45 +0100

    new file:   cypress.json 	new file:   cypress/fixtures/example.json 	new file:   cypress/integration/login/login-spec.js 	new file:   cypress/integration/login/login.cy.js 	new file:   cypress/integration/login/no-password-spec.js 	new file:   cypress/plugins/index.js 	new file:   cypress/support/commands.js 	new file:   cypress/support/index.js 	modified:   package-lock.json 	modified:   package.json

---
+++ cypress/integration/login/login-spec.js
@@ -0,0 +1,10 @@
+it('logs in', () => {
+    cy.visit('/login')
+    cy.get('[name=username]').type('jack')
+    cy.get('[name=password]').type('secret')
+    cy.get('[type=Submit]').click()
+  
+    cy.contains('a', 'profile').should('be.visible').click()
+    cy.url().should('match', /profile$/)
+    cy.contains('Email: jack@example.com')
+  })
---
+++ cypress/integration/login/login.cy.js
@@ -0,0 +1,7 @@
+describe(`login`, () => {
+    it(`does something to login`, () => {
+        cy.visit('http://localhost:3000')
+        cy.get('a').click()
+        
+    })
+})
